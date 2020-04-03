0f2e6662220877a02ba7a5d52731caa0daa980b3
Xavi Borja <xavier.borja@latostadora.com>
2020-04-03 20:37:44 +0200

    First version of calculator with regulatory base between limits

---
+++ assets/javascript/main.js
@@ -0,0 +1,19 @@
+$(function() {
+    $('#calculator').on('submit', function (event) {
+        event.preventDefault();
+        const regulatoryBase = $('#regulatory_base').val();
+        const childNumber = parseInt($('#child_number').val());
+
+        const minimumWithNoChildren = 501.98;
+        const maximumWithNoChildren = 1089.09;
+
+        const baseFirstSixMonths = regulatoryBase * 0.7;
+        const baseNextMonths = regulatoryBase * 0.5;
+        console.log(childNumber,baseFirstSixMonths);
+        if (childNumber === 0 && (maximumWithNoChildren > baseFirstSixMonths && baseFirstSixMonths > minimumWithNoChildren)){
+            $('#result_first_six_months').html(baseFirstSixMonths + ' €');
+            $('#result_following_months').html(baseNextMonths + ' €');
+            $('#result').show();
+        }
+    });
+});
