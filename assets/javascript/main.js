07de765eb43e7ff452e4e6b75f8599a254efc678
Xavi Borja <xavier.borja@latostadora.com>
2020-04-03 23:38:30 +0200

    Reset functionality + calc with one children

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -5,12 +5,21 @@ $(function() {
         const childNumber = parseInt($('#child_number').val());
 
         const minimumWithNoChildren = 501.98;
+        const minimumWithOneOrMoreChildren = 671.40;
         const maximumWithNoChildren = 1089.09;
+        const maximumWithOneChildren = 1254.96;
+        const maximumWithTwoOrMoreChildren = 1411.83;
 
         const baseFirstSixMonths = regulatoryBase * 0.7;
         const baseNextMonths = regulatoryBase * 0.5;
-        console.log(childNumber,baseFirstSixMonths);
+
         if (childNumber === 0 && (maximumWithNoChildren > baseFirstSixMonths && baseFirstSixMonths > minimumWithNoChildren)){
+            $('#result_first_six_months').html(baseFirstSixMonths + ' €');
+            $('#result_following_months').html(baseNextMonths + ' €');
+            $('#result').show();
+            return;
+        }
+        if (childNumber === 1 && (maximumWithOneChildren > baseFirstSixMonths && baseFirstSixMonths > minimumWithOneOrMoreChildren)){
             $('#result_first_six_months').html(baseFirstSixMonths + ' €');
             $('#result_following_months').html(baseNextMonths + ' €');
             $('#result').show();
@@ -22,5 +31,17 @@ $(function() {
             $('#result').show();
             return;
         }
+        if (childNumber === 1 && baseFirstSixMonths > maximumWithOneChildren){
+            $('#result_first_six_months').html(maximumWithOneChildren + ' €');
+            $('#result_following_months').html(maximumWithOneChildren + ' €');
+            $('#result').show();
+            return;
+        }
+    });
+
+    $('#reset').on('click', function () {
+        $('#result').hide();
+        $('#result_first_six_months').empty();
+        $('#result_following_months').empty();
     });
 });
